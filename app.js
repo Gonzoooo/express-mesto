7041ade7a31cfbdedffb67873601f53a9f99c192
Olga Smirnova <g.o.n.z.o.m.e@yandex.ru>
2021-09-06 18:32:55 +0300

    добавлены модели, контроллеры, роутеры

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
-// Слушаем 3000 порт
+const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`App listening on port ${PORT}`)
-})
+});
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(3000);
+
+app.get('/users', (req, res) => {
+  res.send(req.params);
+});
+
+app.get('/users/:userId', (req, res) => {
+  res.send(req.params);
+});
+
+app.post('/users', (req, res) => {
+  const { name, about, avatar } = req.params;
+  res.send({name, about, avatar});
+});
